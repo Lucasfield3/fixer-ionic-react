60fb9770125513c106c49aa1ac1a01c04dbfc240
Lucasfield3 <lucasclic@yahho.com.br>
2020-09-04 14:01:53 -0300

    comitado2

--- src/pages/Landing/landing.tsx
+++ src/pages/Landing/landing.tsx
@@ -1,25 +1,86 @@
-import React from 'react';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonCard, IonCardContent, IonButton, IonRow, IonGrid } from '@ionic/react';
+import React, { useState } from 'react';
+import {
+IonPage,
+IonHeader,
+IonToolbar,
+IonTitle,
+IonContent,
+IonItem,
+IonLabel,
+IonCard,
+IonCardContent,
+IonButton,
+IonRow,
+IonGrid,
+IonInput,
+IonCol,
+IonImg
+} from '@ionic/react';
 import './style.css'
+import ReactCardFlip from 'react-card-flip';
+import logo from '../../Assets/palavra-logo1.png'
 
 const Landing: React.FC = ()=>{
-
+const [isFlipped, setIsflipped] = useState(false);
+const handleClick = ()=>{
+    setIsflipped(!isFlipped)
+}
     return(
         <IonPage className="landing">
+            <IonImg className='logo' alt='logo' src={logo}></IonImg>
             <IonContent >
-                <IonGrid >
+                <IonGrid>
                 <IonRow  className="ion-justify-content-center ion-margin-top">
                     <IonCard className='card' color='light'>
-                        <IonCardContent></IonCardContent>
+                        <IonCardContent ></IonCardContent>
                     </IonCard>
                 </IonRow>
                 <IonRow className="ion-justify-content-center">
-                    <IonCard  color='light'>
-                        <IonCardContent>
-                            <IonButton className='ion-margin'>Login</IonButton>
-                            <IonButton className='ion-margin'>Cadastro</IonButton>
-                        </IonCardContent>
-                    </IonCard>
+                    <ReactCardFlip isFlipped={isFlipped} flipDirection='horizontal'>
+                        <IonCard className='card-form' color='light'>
+                            <IonCardContent>
+                                <IonRow className="ion-align-items-center row">
+                                    <IonCol>
+                                        <IonButton
+                                        onClick={handleClick} 
+                                        size="small" color='dark' 
+                                        className='ion-margin btn-style-dark'
+                                        >Login</IonButton>
+                                    </IonCol>
+                                    <IonCol>
+                                        <IonButton
+                                        onClick={handleClick} 
+                                        size="small" 
+                                        color='primary' 
+                                        className='ion-margin btn-style-light'
+                                        >Cadastro</IonButton>
+                                    </IonCol>
+                                </IonRow>
+                            </IonCardContent>
+                        </IonCard>
+                        <IonCard className='card-form'color='light'>
+                            <IonCardContent>
+                                <IonRow className="ion-align-items-center row">
+                                        <IonCol>
+                                            <IonButton 
+                                            onClick={handleClick} 
+                                            size="small"  
+                                            className='ion-margin btn-style-dark'
+                                            color='dark'
+                                            >Login</IonButton>
+                                        </IonCol>
+                                        <IonCol>
+                                        <IonButton 
+                                        onClick={handleClick} 
+                                        size="small" 
+                                        color='primary'
+                                        className='ion-margin btn-style-light'
+                                        >Cadastro</IonButton>
+                                        </IonCol>
+                                </IonRow>
+                            </IonCardContent>
+                        </IonCard>
+                    </ReactCardFlip>
                 </IonRow>
                 </IonGrid>
             </IonContent>
