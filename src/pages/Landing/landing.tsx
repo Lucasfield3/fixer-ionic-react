6a5535d885d5c0ac417ccebf2351774291cd1feb
Lucasfield3 <lucasclic@yahho.com.br>
2020-09-03 22:47:44 -0300

    comiiit

--- src/pages/Landing/landing.tsx
+++ src/pages/Landing/landing.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonCard, IonCardContent } from '@ionic/react';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonCard, IonCardContent, IonButton, IonRow, IonGrid } from '@ionic/react';
 import './style.css'
 
 const Landing: React.FC = ()=>{
 
     return(
         <IonPage className="landing">
-            <IonContent className="container">
-                <IonCard className="card" color='light'>
-                    <IonCardContent></IonCardContent>
-                </IonCard>
+            <IonContent >
+                <IonGrid >
+                <IonRow  className="ion-justify-content-center ion-margin-top">
+                    <IonCard className='card' color='light'>
+                        <IonCardContent></IonCardContent>
+                    </IonCard>
+                </IonRow>
+                <IonRow className="ion-justify-content-center">
+                    <IonCard  color='light'>
+                        <IonCardContent>
+                            <IonButton className='ion-margin'>Login</IonButton>
+                            <IonButton className='ion-margin'>Cadastro</IonButton>
+                        </IonCardContent>
+                    </IonCard>
+                </IonRow>
+                </IonGrid>
             </IonContent>
         </IonPage>
     );
