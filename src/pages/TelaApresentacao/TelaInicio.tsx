ac21dc0d627d78338b887e2484c6b9c06ddac108
Lucasfield3 <lucasclic@yahho.com.br>
2020-09-11 13:47:45 -0300

    menu lateral logica pronta

--- src/pages/TelaApresentacao/TelaInicio.tsx
+++ src/pages/TelaApresentacao/TelaInicio.tsx
@@ -1,59 +1,70 @@
-import React from 'react';
-import { IonButton, IonPage, IonContent, IonGrid, IonRow, IonCol, IonFab, IonFabButton, IonIcon, IonFabList, IonList, IonItem, IonThumbnail, IonImg, IonLabel } from '@ionic/react'
+import React, { useState } from 'react';
+import { IonButton, IonPage, IonContent, IonGrid, IonRow, IonCol, IonFab, IonFabButton, IonIcon, IonFabList, IonList, IonItem, IonThumbnail, IonImg, IonLabel, IonRouterOutlet, IonHeader, IonMenuButton, IonMenu, IonTitle, IonToolbar, IonApp } from '@ionic/react'
 import { add, settings, share, person, arrowForwardCircle, arrowBackCircle, arrowUpCircle, logoVimeo, logoFacebook, logoInstagram, logoTwitter, menuOutline } from 'ionicons/icons';
-
 import './style.css'
-
-type Item = {
-  src: string;
-};
-const items: Item[] = [{ src: 'http://placekitten.com/g/200/300' }];
-
-
+import { menuController } from '@ionic/core';
+import Button from '../Landing/styles/Button'
 const TelaInicio: React.FC = () => {
+  async function openMenu() {
+    await menuController.open();
+  }
+  async function closeMenu() {
+    await menuController.close();
+  }
+  
   return (
     <>
-      <IonPage className="page-inicio">
-
-        <IonContent>
-
-          <IonFab vertical="top" horizontal="start" slot="fixed">
-            <IonFabButton className="icon-fab-button icone" size="small" color="dark">
-              <IonIcon icon={menuOutline} />
-            </IonFabButton>
-            <IonFabList side="bottom">
-              <IonFabButton className="icon-fab-button-list"><IonIcon icon={logoTwitter} /></IonFabButton>
-            </IonFabList>
-          </IonFab>
-
-
-          <IonGrid>
-
-            <IonRow className="first-row-buttons">
-              <IonCol className="col-left" size="2">PARTE DA ESQUERDA"
-              <IonButton size="small" className="button-flashcards" color="light">Flashcards</IonButton>
-                <IonButton size="small" className="button-questionarios" color="light">Questionarios</IonButton>
-                <IonButton size="small" className="button-classes" color="light">Classes</IonButton>
-                <IonButton size="small" className="button-conquistas" color="light">Conquistas</IonButton>
-
-
-
-
-              </IonCol>
-
-
-              <IonCol className="col-right" size="3" offset="3">
-                PARTE DA DIREITA
-              </IonCol>
+    <IonApp>
+      <IonMenu className='custom-menu' type="overlay" side='start'contentId="main-content">
+          <IonHeader>
+              <IonToolbar color="primary">
+                <IonFabButton slot='start' onClick={closeMenu} className="icon-fab-button light" size="small" color='light'>
+                  <IonIcon icon={menuOutline} />
+                </IonFabButton>
+              </IonToolbar>
+          </IonHeader>
+          <IonContent>
+            <IonRow className='ion-justify-content-center menu-items'>
+              <IonRow>  
+                <Button size="small"  color="light">Flashcards</Button>
+              </IonRow>
+              <IonRow>
+                <Button size="small"  color="light">Questionarios</Button>      
+              </IonRow>
+              <IonRow>
+                <Button size="small"  color="light">Classes</Button>    
+              </IonRow>
+              <IonRow>
+                <Button size="small"  color="light">Conquistas</Button>
+              </IonRow>
+              <IonRow>
+              <Button size="small"  color="light">Sair</Button>
+              </IonRow>          
             </IonRow>
+          </IonContent>
+      </IonMenu>
 
-
-
-          </IonGrid>
+      <IonPage id="main-content" className="page-inicio">
+        <IonHeader>
+          <IonToolbar>
+          <IonFabButton  slot='start' onClick={openMenu} className="icon-fab-button icone" size="small" color="dark">
+            <IonIcon icon={menuOutline} />
+            <IonButton slot='start'>
+              <IonMenuButton></IonMenuButton>
+            </IonButton>
+          </IonFabButton>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
+          <IonFabButton slot='start' onClick={openMenu} className="icon-fab-button icone" size="small" color="dark">
+            <IonIcon icon={menuOutline} />
+          </IonFabButton>
         </IonContent>
       </IonPage>
+    </IonApp>
     </>
   )
 }
+
 
 export default TelaInicio
