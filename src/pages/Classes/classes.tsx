c2bdae4262ccf5567e2caab610fed0f1abab8c40
Lucasfield3 <lucasclic@yahho.com.br>
2020-09-30 19:31:11 -0300

    mergiado

--- src/pages/Classes/classes.tsx
+++ src/pages/Classes/classes.tsx
@@ -47,52 +47,6 @@ const Classes: React.FC = () => {
                     </IonToolbar>
                 </IonHeader>
 
-<<<<<<< HEAD
-                <IonContent >
-                    <IonButton className="btnMostrar" onClick={() => setShowActionSheet(true)} expand="block">
-                        Add
-                    </IonButton>
-
-                    <IonActionSheet
-                        isOpen={showActionSheet}
-                        onDidDismiss={() => setShowActionSheet(false)}
-                        cssClass='my-custom-class'
-                        buttons={[{
-                            text: 'Delete',
-                            role: 'destructive',
-                            icon: trash,
-                            handler: () => {
-                                console.log('Delete clicked');
-                            }
-                        }, {
-                            text: 'Share',
-                            icon: share,
-                            handler: () => {
-                                console.log('Share clicked');
-                            }
-                        }, {
-                            text: 'Play (open modal)',
-                            icon: caretForwardCircle,
-                            handler: () => {
-                                console.log('Play clicked');
-                            }
-                        }, {
-                            text: 'Favorite',
-                            icon: heart,
-                            handler: () => {
-                                console.log('Favorite clicked');
-                            }
-                        }, {
-                            text: 'Cancel',
-                            icon: close,
-                            role: 'cancel',
-                            handler: () => {
-                                console.log('Cancel clicked');
-                            }
-                        }]}
-                    >
-                    </IonActionSheet>
-=======
                 <IonContent>
                     <IonFab vertical="bottom" horizontal="center" slot="fixed" color="dark">
                         <IonFabButton>
@@ -104,10 +58,7 @@ const Classes: React.FC = () => {
                             <IonButton className="ButtonChoise" size="small" color="dark">Alternativa</IonButton>
                         </IonFabList>
                     </IonFab>
->>>>>>> 6450bf99b7aa927e5242c6d3e01b05e87aaef5f2
                 </IonContent>
-
-
             </IonPage>
         </>
     );
