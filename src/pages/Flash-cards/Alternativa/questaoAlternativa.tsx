9b4ea2cfbbbfd56d8d0def67b87808b4e8cb32aa
Lucasfield3 <lucasclic@yahho.com.br>
2020-10-17 00:05:26 -0300

    alternativas estilizada

--- src/pages/Flash-cards/Alternativa/questaoAlternativa.tsx
+++ src/pages/Flash-cards/Alternativa/questaoAlternativa.tsx
@@ -17,6 +17,7 @@ import './style.css'
 import { menuController } from '@ionic/core';
 import { useHistory } from 'react-router';
 import styled from 'styled-components';
+import { stringify } from 'querystring';
 
 
 const QuestaoAlternativa: React.FC = () => {
@@ -32,16 +33,14 @@ const QuestaoAlternativa: React.FC = () => {
     const [shownTimer, setShownTimer] = useState<boolean>(false);
     const [showPopover, setShowPopover] = useState<boolean>(false);
     const [shownPopsave, setShownPopsave]= useState<boolean>(false);
-    const [showPopChoose, setShowPopChoose] = useState<boolean>(false);
-    const [shownPopRight, setShownPopRight] = useState<boolean>(false);
     const [textPop, setTextPop] = useState<string>('')
     const [showModal, setShowModal] = useState(false)
     const [showModal2, setShowModal2] = useState(false)
-    const [textTarget, setTextTarget] = useState<string>('')
-    const [textBtn, setTextBtn] = useState('Definir essa quantidade')
-    const [btnShow, setBtnShow] = useState<{}>(<ButtonPop showPop={()=>setShowPopChoose(true)}>{textBtn}</ButtonPop>)
+    const [textRightAnswer, setTextRightAnswer] = useState<string>('')
 
     
+
+  
     const temas = {
         id: 0,
         textPop: ''
@@ -82,7 +81,7 @@ const QuestaoAlternativa: React.FC = () => {
 
         console.log(alternatives)
         
-        if(alternatives.length == 5){
+        if(alternatives.length == 4){
             setAlternatives(alternatives)
 
         }
@@ -110,13 +109,7 @@ const QuestaoAlternativa: React.FC = () => {
         setAlternatives([])
 
     }, [])
-const ShowBtn = ()=>{
-    if(alternatives.length > 0){
-        setBtnShow(<ButtonPop showPop={()=> {
-            setShowPopChoose(true)
-        }}>{textBtn}</ButtonPop>)
-    }
-}
+
     const CleanInputs = () => {
         setTextPop('')
         setTextAreaAlternative('')
@@ -125,37 +118,6 @@ const ShowBtn = ()=>{
         setTextTitle('')
     }
 
-    const PickRightAnswer = ()=>{
-        const alternativas = Array.from(document.getElementsByClassName('alternativas-textarea') as HTMLCollectionOf<HTMLElement>)
-        alternativas.length = alternatives.length
-        alternativas.forEach(item =>{
-           item.addEventListener('click', ()=>{
-            item.style.border = '1px #000 solid'
-         } ,true)
-       });
-       setShowPopChoose(false)
-    }
-    const DisableRemove = ()=>{
-        const btnRemove =  Array.from(document.getElementsByClassName('remove-btn') as HTMLCollectionOf<HTMLButtonElement>)
-           btnRemove.forEach(button =>{
-                button.disabled = true
-           })
-    }
-    const AbleRemove = ()=>{
-        const btnRemove =  Array.from(document.getElementsByClassName('remove-btn') as HTMLCollectionOf<HTMLButtonElement>)
-           btnRemove.forEach(button =>{
-                button.disabled = false
-           })
-    }
-    const AbleAdd = ()=>{
-        const addAlternativas = document.getElementById('add-alternative') as HTMLButtonElement
-        return addAlternativas.disabled == false
-    }
-    const DisableAdd = ()=>{
-        const addAlternativas = document.getElementById('add-alternative') as HTMLButtonElement
-        return addAlternativas.disabled == true
-    }
-       
 
     return (
         <>
@@ -282,45 +244,25 @@ const ShowBtn = ()=>{
                     </IonModal>
 
 
-                    <IonGrid>
-                        <IonRow className='ion-justify-content-center'>
-                                <IonTextarea autoGrow={true} className='ios add-temas' placeholder='Insira as alternativas' color='dark'  onIonChange={e => setTextAreaAlternative(e.detail.value!)} value={textAreaAlternative}></IonTextarea>
+                    <IonGrid className='array-div'>
+                    <IonRow style={{marginBottom:'1rem'}} className='ion-justify-content-center'>
+                                <IonTextarea autoGrow={textRightAnswer == '' && false || true} style={{height: textRightAnswer == '' && '4rem' || 'auto'}} className='ios alternativa-correta' placeholder='Insira a alternativa correta' color='dark'  onIonChange={e => setTextRightAnswer(e.detail.value!)} value={textRightAnswer}></IonTextarea>
+                    </IonRow>
+                        <IonRow  className='ion-justify-content-center'>
+                                <IonTextarea autoGrow={true} className='ios add-alternativas'  placeholder='Insira a/as alternativas' color='dark'  onIonChange={e => setTextAreaAlternative(e.detail.value!)} value={textAreaAlternative}></IonTextarea>
                                 <IonFabButton id='add-alternative' className='add-btn'  onClick={()=> {
                                     AddAlternative()
                                     setTextAreaAlternative('')
                                     }} color='light'><IonIcon color='success' icon={add}></IonIcon></IonFabButton>
-                        </IonRow>                                        
+                        </IonRow>                       
                             {alternatives.map((alternative, index)=>(
-                                <IonRow key={index} style={{cursor:'default', marginTop:'1rem'}}  className='ion-justify-content-center'>
-                                    <IonCol style={{height:'auto', width:'10rem'}} key={alternative.id} onFocus={(e)=>{
-                                            e.currentTarget.style.border = '2px var(--ion-color-danger) solid'
-                                    }}   className='ios alternativas-textarea' color='dark' placeholder='alternativas'>{alternative.textAreaAlternative}</IonCol>
+                                <IonRow key={index} style={{cursor:'default', marginTop:'1rem'}}  className='ion-justify-content-center colunas'>
+                                    <IonCol style={{height:'auto', width:'10rem'}} key={alternative.id} className='alternativas' color='dark' placeholder='alternativas'>{alternative.textAreaAlternative}</IonCol>
                                     <IonFabButton  onClick={()=>DeleteAlternatives(alternative.id)} className='remove-btn'  color='light'><IonIcon color='danger' icon={remove}></IonIcon></IonFabButton>
                                 </IonRow>
-                            ))}
+                            ))}                       
                     </IonGrid>
-                        {alternatives.length > 1 && btnShow}
-                        <PopRightAnswer isOpen={shownPopRight} ablePick={()=>{
-                            PickRightAnswer()
-                            setShownPopRight(false)
-                            }} hidePopover={()=>setShownPopRight(false) }/>
-                        <PopChoose hidePopover={()=>{
-                            setShowPopChoose(false)
-                            }} 
-                            isOpen={showPopChoose}
-                            ablePick={()=>{
-                                setShownPopRight(true)
-                                DisableRemove()
-                                DisableAdd()
-                                setBtnShow('')
-                            }}
-                            disablePick={()=>{
-                                setShowPopChoose(false)
-                                AbleRemove()
-                                AbleAdd()                        
-                            }}
-                            >
-                        </PopChoose>
+                       
                     <IonRow className='row-toggle'>                                            
                         <IonLabel color='dark' className='label-timer' >Tempo</IonLabel>                        
                         <IonToggle checked={checked} onIonChange={(e)=>setChecked(e.detail.checked)} className='ios toggle' onClick={()=>setShownTimer(!shownTimer)}/>
@@ -371,59 +313,6 @@ const Timer: React.FC = () => {
         </>
     );
 }
-
-const PopChoose:React.FC<{isOpen:boolean; hidePopover:()=>void;disablePick:()=>void;ablePick:()=>void}> = props=>{
-
-    return(
-        <>
-             <IonPopover
-                isOpen={props.isOpen}
-                cssClass='my-custom-class quantidade'
-                onDidDismiss={props.hidePopover}
-            >
-                <IonRow  className='ion-justify-content-center ion-text-align-center'>
-                    <IonLabel style={{fontWeight:'bold', fontSize:'16px'}} color='dark'>Tem certeza dessa quantidade?</IonLabel>
-                </IonRow>
-                <IonRow  className='ios ion-justify-content-center'>
-                    <IonButton id='sim' className="ios yes" color='success' onClick={props.ablePick} >Sim</IonButton>
-                    <IonButton className="ios no" color='danger' onClick={props.disablePick} >Não</IonButton>
-                </IonRow>
-            </IonPopover>
-        </>
-    );
-}
-const ButtonPop:React.FC<{showPop:()=>void}> = props =>{
-
-    return(
-        <>
-            <IonRow  className='ios ion-justify-content-center'>
-    <IonButton className="ios btn-quantidade" color='light'  onClick={props.showPop} >{props.children}</IonButton>
-            </IonRow>
-            
-        </>
-    );
-}
-const PopRightAnswer:React.FC<{isOpen:boolean; hidePopover:()=>void;ablePick:()=>void}> = props=>{
-
-    return(
-        <>
-             <IonPopover
-                isOpen={props.isOpen}
-                cssClass='my-custom-class quantidade'
-                onDidDismiss={props.hidePopover}
-            >
-                <IonRow  className='ion-justify-content-center ion-text-align-center'>
-                    <IonLabel style={{fontWeight:'bold', fontSize:'16px'}} color='dark'>Agora escolha a alternativa certa.</IonLabel>
-                </IonRow>
-                <IonRow  className='ios ion-justify-content-center'>
-                    <IonButton id='Okay' className="ios ok-btn" color='success' onClick={props.ablePick} >Ok</IonButton>
-                   
-                </IonRow>
-            </IonPopover>
-        </>
-    );
-}
-
 
 
 export default QuestaoAlternativa;
